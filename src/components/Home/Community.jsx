7dbdbb7a63dd047417581041f964fad23ffe0ee2
Gifty Arthur <giftyarthur031@gmail.com>
2024-07-26 21:13:51 +0000

    update

--- src/components/Home/Community.jsx
+++ src/components/Home/Community.jsx
@@ -1,3 +1,5 @@
+import Medicine from "./Medicine";
+
 //images
 import ebg1 from "../../assets/Images/Education/ebg1.png";
 import ed2 from "../../assets/Images/Education/ed2.png";
@@ -5,106 +7,107 @@ import ed2 from "../../assets/Images/Education/ed2.png";
 const community = () => {
   return (
     <div>
-      <div className="relative h-[500px]">
-        <img src={ebg1} alt=" " className="md:h-auto  h-[910px]" />
-
-        <div className="absolute inset-0 ">
-          <div className="container">
-            <div className="md:flex md:flex-row gap-10 md:mt-20 mt-4 ">
-              <div className="flex flex-col">
-                <p className="font-sue text-white md:text-[35px] ">
-                  How we Help
-                </p>
-                <h1 className="font-mincho text-white font-extrabold md:text-4xl text-md mt-2">
-                  Join The Community To <br /> Give Education For
-                  <br /> Children
-                </h1>
-                <p className="text-white text-sm md:mt-2 mt-1 font-work">
-                  Lorem Ipsum is simply dummy text of the printin typesetting
-                  <br />
-                  dummy text ever when an unknown printer took a galley of type
-                  <br />
-                  and scrambled it to make a type specimen book.
-                </p>
-                <img src={ed2} alt="" className="mt-4" />
-                <a href="" target="_blank" rel="noopener noreferrer">
-                  <button className=" w-[115px] h-[45px] text-white bg-primary md:text-md text-sm rounded-sm  hover:bg-gray-500 hover:text-white md:mt-8 mt-8 ">
-                    DONATE NOW
-                  </button>
-                </a>
-              </div>
-              {/* becom */}
-              <div className="md:w-[569px] md:h-[529px] w-[399px] h-[410px] border-2 md:mt-0 mt-6">
-                <form className="max-w-sm mx-auto px-4 md:mt-16">
-                  <h1 className="text-white font-mincho text-2xl mt-4 font-extrabold">
-                    Become A Volunteer
+      <div>
+        <div className="relative ">
+          <img src={ebg1} alt=" " className="md:h-auto  h-[910px]" />
+          <div className="absolute inset-0 ">
+            <div className="container">
+              <div className="md:flex md:flex-row gap-10 md:mt-20 mt-4 ">
+                <div className="flex flex-col">
+                  <p className="font-sue text-white md:text-[35px] ">
+                    How we Help
+                  </p>
+                  <h1 className="font-mincho text-white font-extrabold md:text-4xl text-md mt-2">
+                    Join The Community To <br /> Give Education For
+                    <br /> Children
                   </h1>
-                  <div className="mb-5">
-                    <label
-                      htmlFor="FullName"
-                      className="block mb-2 text-sm font-medium text-white dark:text-white"
-                    ></label>
-                    <input
-                      type="name"
-                      id="name"
-                      className=" border border-white  text-sm block w-[359px] p-2.5 bg-transparent  font-work text-md dark:border-white dark:placeholder-white dark:text-white  "
-                      placeholder="Full Name*"
-                      required
-                    />
-                  </div>
-                  <div className="mb-5">
-                    <label
-                      htmlFor="email"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    ></label>
-                    <input
-                      type="email"
-                      id="email"
-                      className=" border border-white  text-sm block w-[359px] p-2.5 bg-transparent  font-work text-md dark:border-white dark:placeholder-white dark:text-white  "
-                      placeholder="Email Adress*"
-                      required
-                    />
-                  </div>
-
-                  <div className="mb-5">
-                    <label
-                      htmlFor="number"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    ></label>
-                    <input
-                      type="number"
-                      id="number"
-                      className=" border border-white  text-sm block w-[359px] p-2.5 bg-transparent  font-work text-md dark:border-white dark:placeholder-white dark:text-white  "
-                      placeholder="Phone Number"
-                      required
-                    />
-                  </div>
-                  <div className="mb-5">
-                    <label
-                      htmlFor=""
-                      className=" text-sm font-medium text-white dark:text-white"
-                    ></label>
-                    <input
-                      type=""
-                      id=""
-                      className=" border border-white  text-sm block w-[359px] h-[90px] p-2.5 bg-transparent  font-work text-md dark:border-white dark:placeholder-white dark:text-white  "
-                      placeholder="Message"
-                      required
-                    />
-                  </div>
-
-                  <button
-                    type="submit"
-                    className="text-tertiary bg-white hover:bg-blue-800 focus:ring-4 focus:outline-none  font-medium  text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-white dark:hover:bg-primary rounded-full md:mt-4"
-                  >
-                    Submit
-                  </button>
-                </form>
+                  <p className="text-white text-sm md:mt-2 mt-1 font-work">
+                    Lorem Ipsum is simply dummy text of the printin typesetting
+                    <br />
+                    dummy text ever when an unknown printer took a galley of
+                    type
+                    <br />
+                    and scrambled it to make a type specimen book.
+                  </p>
+                  <img src={ed2} alt="" className="mt-4" />
+                  <a href="" target="_blank" rel="noopener noreferrer">
+                    <button className=" w-[115px] h-[45px] text-white bg-primary md:text-md text-sm rounded-sm  hover:bg-gray-500 hover:text-white md:mt-8 mt-8 ">
+                      DONATE NOW
+                    </button>
+                  </a>
+                </div>
+                {/* becom */}
+                <div className="md:w-[569px] md:h-[529px]  w-full h-[410px] border-2 md:mt-0 mt-6">
+                  <form className="max-w-sm mx-auto px-4 md:mt-16">
+                    <h1 className="text-white font-mincho text-2xl mt-4 font-extrabold">
+                      Become A Volunteer
+                    </h1>
+                    <div className="mb-5">
+                      <label
+                        htmlFor="FullName"
+                        className="block mb-2 text-sm font-medium text-white dark:text-white"
+                      ></label>
+                      <input
+                        type="name"
+                        id="name"
+                        className=" border border-white  text-sm block md:w-[359px] w-full p-2.5 bg-transparent  font-work text-md dark:border-white dark:placeholder-white dark:text-white  "
+                        placeholder="Full Name*"
+                        required
+                      />
+                    </div>
+                    <div className="mb-5">
+                      <label
+                        htmlFor="email"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      ></label>
+                      <input
+                        type="email"
+                        id="email"
+                        className=" border border-white  text-sm block md:w-[359px] w-full p-2.5 bg-transparent  font-work text-md dark:border-white dark:placeholder-white dark:text-white  "
+                        placeholder="Email Adress*"
+                        required
+                      />
+                    </div>
+                    <div className="mb-5">
+                      <label
+                        htmlFor="number"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      ></label>
+                      <input
+                        type="number"
+                        id="number"
+                        className=" border border-white  text-sm block md:w-[359px] w-full  p-2.5 bg-transparent  font-work text-md dark:border-white dark:placeholder-white dark:text-white  "
+                        placeholder="Phone Number"
+                        required
+                      />
+                    </div>
+                    <div className="mb-5">
+                      <label
+                        htmlFor=""
+                        className=" text-sm font-medium text-white dark:text-white"
+                      ></label>
+                      <input
+                        type=""
+                        id=""
+                        className=" border border-white  text-sm block md:w-[359px] w-full  h-[90px] p-2.5 bg-transparent  font-work text-md dark:border-white dark:placeholder-white dark:text-white  "
+                        placeholder="Message"
+                        required
+                      />
+                    </div>
+                    <button
+                      type="submit"
+                      className="text-tertiary bg-white hover:bg-blue-800 focus:ring-4 focus:outline-none  font-medium  text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-white dark:hover:bg-primary rounded-full md:mt-4"
+                    >
+                      Submit
+                    </button>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <Medicine />
     </div>
   );
 };
---
+++ src/components/Home/Medicine.jsx
@@ -0,0 +1,122 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+import icon2 from "../../assets/Images/Education/icon2.png";
+import icon3 from "../../assets/Images/Education/icon3.png";
+import icon4 from "../../assets/Images/Education/icon4.png";
+import icon5 from "../../assets/Images/Education/icon5.png";
+import icon6 from "../../assets/Images/Education/icon6.png";
+import icon from "../../assets/Images/little/icon.png";
+function Medicine() {
+  return (
+    <div>
+      <div className="container">
+        <div className="text-center mt-10">
+          <p className="font-sue text-primary md:text-[35px] ">What we do</p>
+          <h1 className="font-mincho font-extrabold md:text-4xl text-md">
+            We do it for People in Need
+          </h1>
+        </div>
+        {/* icons */}
+        <div className="mt-10">
+          <div className="flex flex-col md:gap-10 ">
+            <div className="md:flex items-center justify-between">
+              <div className="md:w-[260px] md:h-[200px] w-full ">
+                <div className=" flex-col flex items-center justify-center">
+                  <img src={icon} alt="" />
+                  <h3 className="font-extrabold font-mincho text-[25px] text-tertiary">
+                    Medicine Help
+                  </h3>
+                  <p className="text-[#727272] text-md font-work mt-2">
+                    Centuries but also the leap into <br />
+                    electronic typesetting, remaining
+                    <br />
+                    <span className="ml-10"> specimen book.</span>
+                  </p>
+                </div>
+              </div>
+              <div className="md:w-[260px] md:h-[200px] w-full md:mt-0 mt-4">
+                <div className=" flex-col flex items-center justify-center">
+                  <img src={icon2} alt="" />
+                  <h3 className="font-extrabold font-mincho md:text-[25px] text-tertiary">
+                    Water Delivery
+                  </h3>
+                  <p className="text-[#727272] md:text-md font-work mt-2 text-sm">
+                    Centuries but also the leap into <br />
+                    electronic typesetting, remaining
+                    <br />
+                    <span className="ml-10"> specimen book.</span>
+                  </p>
+                </div>
+              </div>
+              {/* 3rd */}
+              <div className="md:w-[260px] md:h-[200px] w-full md:mt-0 mt-10 ">
+                <div className=" flex-col flex items-center justify-center">
+                  <img src={icon3} alt="" />
+                  <h3 className="font-extrabold font-mincho text-[25px] text-tertiary">
+                    We Care About
+                  </h3>
+                  <p className="text-[#727272] text-md font-work mt-2">
+                    Centuries but also the leap into <br />
+                    electronic typesetting, remaining
+                    <br />
+                    <span className="ml-10"> specimen book.</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            {/* 4th */}
+            <div className="md:flex items-center justify-between">
+              <div className="md:w-[260px] md:h-[200px] w-full md:mt-0 mt-10 ">
+                <div className=" flex-col flex items-center justify-center">
+                  <img src={icon4} alt="" />
+                  <h3 className="font-extrabold font-mincho text-[25px] text-tertiary">
+                    Food Delivery
+                  </h3>
+                  <p className="text-[#727272] text-md font-work mt-2">
+                    Centuries but also the leap into <br />
+                    electronic typesetting, remaining
+                    <br />
+                    <span className="ml-10"> specimen book.</span>
+                  </p>
+                </div>
+              </div>
+              {/* 5th */}
+              <div className="md:w-[260px] md:h-[200px] w-full md:mt-0 mt-10 ">
+                <div className=" flex-col flex items-center justify-center">
+                  <img src={icon5} alt="" />
+                  <h3 className="font-extrabold font-mincho text-[25px] text-tertiary">
+                    Spread The World
+                  </h3>
+                  <p className="text-[#727272] text-md font-work mt-2">
+                    Centuries but also the leap into <br />
+                    electronic typesetting, remaining
+                    <br />
+                    <span className="ml-10"> specimen book.</span>
+                  </p>
+                </div>
+              </div>
+              {/*  6th*/}
+              <div className="md:w-[260px] md:h-[200px] w-full md:mt-0 mt-10 ">
+                <div className=" flex-col flex items-center justify-center">
+                  <img src={icon6} alt="" />
+                  <h3 className="font-extrabold font-mincho text-[25px] text-tertiary">
+                    Learn Education
+                  </h3>
+                  <p className="text-[#727272] text-md font-work mt-2">
+                    Centuries but also the leap into <br />
+                    electronic typesetting, remaining
+                    <br />
+                    <span className="ml-10"> specimen book.</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Medicine;
