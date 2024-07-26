2c09631127d7941b0a99a421048a24129e8fe55d
Gifty Arthur <giftyarthur031@gmail.com>
2024-07-26 13:04:13 +0000

    update

--- src/components/Home/Education.jsx
+++ src/components/Home/Education.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 //icons
 import { MdArrowRight } from "react-icons/md";
@@ -10,9 +11,9 @@ const Education = () => {
     <div className="relative z-20">
       <div className="container">
         {/* educatio  */}
-        <div className="flex flex-row items center justify-center">
-          <div className="bg-secondary md:w-[398px] md:h-[330px] w-[130px] h-[250px] md:-mt-16 -mt-2 shadow-md flex items-center justify-center ">
-            <div className="md:p-6 p-2">
+        <div className="md:flex flex-row items center justify-center">
+          <div className="bg-secondary md:w-[398px] md:h-[330px] w-full h-[250px] md:-mt-16 -mt-2 shadow-md flex items-center justify-center ">
+            <div className="md:p-4 p-2">
               <h1 className="text-tertiary font-extrabold md:text-[35px] font-mincho ">
                 Education
               </h1>
@@ -21,35 +22,35 @@ const Education = () => {
                 economic disadvantage.
               </p>
               <a href="" target="_blank" rel="noopener noreferrer">
-                <button className=" md:w-[115px] w-[85px] md:h-[45px] h-[25px] bg-white md:text-md text-sm rounded-sm text-tertiary hover:bg-primary hover:text-white md:mt-14 mt-1 ">
+                <button className=" w-[115px] h-[45px]  bg-white md:text-md text-sm rounded-sm text-tertiary hover:bg-primary hover:text-white md:mt-14 mt-8 ">
                   DISCOVER
                 </button>
               </a>
             </div>
           </div>
-          {/* volun */}
-          <div className="bg-tertiary lg:w-[398px] md:h-[330px] w-[130px] h-[250px] md:-mt-16 -mt-2 shadow-md  flex items-center justify-center">
-            <div className="md:p-6 p-2">
+          {/* volum */}
+          <div className="bg-tertiary md:w-[398px] md:h-[330px] w-full h-[250px] md:-mt-16 mt-2 shadow-md  flex ">
+            <div className="md:p-6 p-2 md:mt-6">
               <h1 className="text-white font-extrabold md:text-[35px] font-mincho ">
                 Become a Volunteer
               </h1>
-              <p className="text-white text-sm md:mt-2 mt-1 font-work">
+              <p className="text-white text-sm md:mt-2 mt-4 font-work">
                 Below Poverty Line is enchmark used by the government
               </p>
               <a href="" target="_blank" rel="noopener noreferrer">
-                <div className="mt-2 text-white underline font-work text-sm md:mt-4">
+                <div className="mt-8 text-white underline font-work text-sm md:mt-8 ">
                   JOIN ORGANIZATION
                 </div>
               </a>
             </div>
           </div>
           {/* img */}
-          <div className="md:-mt-16 -mt-2 ">
+          <div className="md:-mt-16 mt-2 ">
             <div className="relative">
               <img
                 src={ed1}
                 alt=""
-                className="md:w-[398px] md:h-[330px] w-[130px] h-[250px]"
+                className=" md:w-[398px] md:h-[330px] w-full h-[250px]"
               />
               <div className="absolute inset-0 flex items-center justify-center">
                 <a
@@ -69,10 +70,10 @@ const Education = () => {
         {/* people */}
 
         <div className="mt-14">
-          <div className="flex ">
-            <div className="flex flex-col w-[603px] h-[456px]">
+          <div className="md:flex items-center justify-between ">
+            <div className="flex flex-col ">
               <p className="font-sue text-primary md:text-[35px]">About Us</p>
-              <h1 className="font-mincho font-extrabold md:text-4xl text-2xl">
+              <h1 className="font-mincho font-extrabold md:text-4xl text-md">
                 Help People, <br />
                 Our Main Goals
               </h1>
@@ -83,7 +84,7 @@ const Education = () => {
                 <br />
                 and scrambled it to make a type specimen book.{" "}
               </p>
-              <p className="font-work text-[#727272] text-md mt-4">
+              <p className="font-work text-[#727272] text-md mt-4 ">
                 It has survived not only five centuries, but also the leap into
                 electronic
                 <br /> typesetting, remaining when an unknown printer took a
@@ -91,42 +92,42 @@ const Education = () => {
                 <br /> scrambled it to make a type specimen book. essentially.
               </p>
               <a href="" target="_blank" rel="noopener noreferrer">
-                <button className=" md:w-[115px] w-[85px] md:h-[45px] h-[25px] bg-primary rounded-sm text-white hover:bg-custom3 shadow-md mt-4 ">
-                  DONATE
+                <button className=" w-[115px] h-[45px]  bg-primary md:text-md text-sm rounded-sm text-white hover:bg-primary hover:text-white md:mt-14 mt-8 ">
+                  MORE ABOUT
                 </button>
               </a>
             </div>
             {/* b */}
-            <div className="w-[449px] h-[372px] bg-[#ecf1f0]">
+            <div className=" md:w-[449px] md:h-[362px] bg-[#ecf1f0] w-full ">
               <div className="p-8 mt-8">
                 <div className="flex flex-row gap-8">
                   <div className="flex flex-col">
                     <div className="flex flex-row gap-2">
                       <IoMdCheckmarkCircle className="text-secondary mt-1" />
-                      <p>Food Donation</p>
+                      <p className="text-[#727272] text-sm">Food Donation</p>
                     </div>
                     <div className="flex flex-row gap-2">
                       <IoMdCheckmarkCircle className="text-secondary mt-1" />
-                      <p>Money Donation</p>
+                      <p className="text-[#727272] text-sm">Money Donation</p>
                     </div>
                     <div className="flex flex-row gap-2">
                       <IoMdCheckmarkCircle className="text-secondary mt-1" />
-                      <p>Dress Donation</p>
+                      <p className="text-[#727272]">Dress Donation</p>
                     </div>
                   </div>
                   {/* row */}
                   <div className="flex flex-col">
-                    <div className="flex flex-row gap-2">
+                    <div className="flex flex-row gap-2 ">
                       <IoMdCheckmarkCircle className="text-secondary mt-1" />
-                      <p>Food Donation</p>
+                      <p className="text-[#727272] text-sm">Water Supplay</p>
                     </div>
-                    <div className="flex flex-row gap-2">
+                    <div className="flex flex-row gap-2 ">
                       <IoMdCheckmarkCircle className="text-secondary mt-1" />
-                      <p>Money Donation</p>
+                      <p className="text-[#727272]">Education Donation</p>
                     </div>
-                    <div className="flex flex-row gap-2">
+                    <div className="flex flex-row gap-2 text-[#727272]">
                       <IoMdCheckmarkCircle className="text-secondary mt-1" />
-                      <p>Dress Donation</p>
+                      <p className="text-[#727272] text-sm">Toys Donation</p>
                     </div>
                   </div>
                 </div>
@@ -135,18 +136,22 @@ const Education = () => {
                   <h1 className="font-extrabold font-mincho text-[25px] text-tertiary">
                     Total Donation
                   </h1>
-                  <div className="rounded-full h-1 w-full bg-gray-500">
-                    <div className="bg-primary h-1 w-[300px] rounded-full"></div>
+                  <div className="rounded-full h-1 w-full bg-gray-500 mt-2">
+                    <div className="bg-primary h-1 md:w-[300px] w-[150px] rounded-full"></div>
                   </div>
                   <div className="flex items-center justify-between mt-4">
-                    <p>Collection - $5M</p>
-                    <p>Goal-$10M</p>
+                    <p className="font-mincho font-extrabold text-md text-tertiary">
+                      Collection - $5M
+                    </p>
+                    <p className="font-mincho font-extrabold text-md text-tertiary">
+                      Goal-$10M
+                    </p>
                   </div>
                   <a href="" target="_blank" rel="noopener noreferrer">
-                    <button className=" md:w-[115px] w-[85px] md:h-[45px] h-[25px] bg-white rounded-sm text-black hover:bg-custom3 shadow-md mt-4 ">
+                    <button className=" w-[115px] h-[45px]  bg-white md:text-md text-sm rounded-sm text-tertiary hover:bg-primary hover:text-white md:mt-14 mt-8 ">
                       DONATE
                     </button>
-                  </a>
+                  </a>{" "}
                 </div>
               </div>
             </div>
